254af930b6784b9244ed250a8b162f8ac7e882b2
Evan <srichsun@gmail.com>
2017-11-23 15:49:19 +0800

    2.1 2.2 2.3 滑鼠點圖片，大圖就更換該圖

--- srichsun.js
+++ srichsun.js
@@ -1,21 +1,14 @@
 /*jslint devel: true */
 
-//execute the function once after brower downloaded all the files 
-//一定要寫，否則在瀏覽器沒有下載完HTML時，就跑下面的getElementById，會抓不到。
+
 window.onload = function () {
 
-    function changeToBikini() {
-        this.src = "http://s3.india.com/travel/wp-content/uploads/2016/05/Kate-Upton-main.jpg";
+    function changeMainPicture() {
+        document.getElementById("main_picture").src = this.src;
     }
 
-    function changeToCatwalk() {
-        this.src = "https://pbs.twimg.com/media/CGsNYMLWoAAcOYQ.jpg:large";
-        //this 就是滑鼠指到的物件, 放在function裡面
-    }
-
-    var kateupon_pic = document.getElementById("kateupton");
-    kateupon_pic.onmouseover = changeToBikini;
-    kateupon_pic.onmouseout = changeToCatwalk;
-    // 觸發function不用加()
+    document.getElementById("first_pic").onclick = changeMainPicture;
+    document.getElementById("second_pic").onclick = changeMainPicture;
+    document.getElementById("third_pic").onclick = changeMainPicture;
 
 };
