0da48e19d7f36e72bb45b1b32ea1a9d274312989
Evan <srichsun@gmail.com>
2017-11-27 00:06:31 +0800

    3.6 滑鼠過去就顯示，移開就隱藏

--- srichsun.js
+++ srichsun.js
@@ -4,7 +4,15 @@ window.onload = function () {
     function show() {
         document.getElementById("box" + this.id.substr(3)).style.display = "block";
     }
-    document.getElementById("get1").onclick = show;
-    document.getElementById("get2").onclick = show;
-    document.getElementById("get3").onclick = show;
+
+    function hide() {
+        document.getElementById("box" + this.id.substr(3)).style.display = "none";
+    }
+
+    document.getElementById("get1").onmouseover = show;
+    document.getElementById("get2").onmouseover = show;
+    document.getElementById("get3").onmouseover = show;
+    document.getElementById("get1").onmouseout = hide;
+    document.getElementById("get2").onmouseout = hide;
+    document.getElementById("get3").onmouseout = hide;
 };
