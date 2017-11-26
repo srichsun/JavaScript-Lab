77dfeaffaea04f76d7bf275d604c1cff8f1232e6
Evan <srichsun@gmail.com>
2017-11-26 23:34:43 +0800

    3-3 用.style給元素inline style

--- srichsun.js
+++ srichsun.js
@@ -1,18 +1,14 @@
 window.onload = function () {
 
-    var n = 0;
 
-    function changpic(a) {
-
-        var number = this.id.substr(8);
-        document.getElementById("image").src = "images/number_" + number + ".jpg";
-
-        a.preventDefault();
+    function becomesmall() {
+        document.getElementById("text").style.fontSize = "5px";
     }
 
-    document.getElementById("picture_1").onclick = changpic;
-    document.getElementById("picture_2").onclick = changpic;
-    document.getElementById("picture_3").onclick = changpic;
-    document.getElementById("picture_4").onclick = changpic;
-    document.getElementById("picture_5").onclick = changpic;
+    function becomehuge() {
+        document.getElementById("text").style.fontSize = "50px";
+    }
+
+    document.getElementById("small").onclick = becomesmall;
+    document.getElementById("big").onclick = becomehuge;
 };
