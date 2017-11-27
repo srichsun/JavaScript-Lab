093196eee6b67524789a571a7c6d33f9b7e9fb31
Evan <srichsun@gmail.com>
2017-11-27 15:06:56 +0800

    6-7-2 續6-7-1 簡化，多個Jquery抓取元素合併

--- srichsun.js
+++ srichsun.js
@@ -2,20 +2,17 @@ $(function () {
 
     $('#sportbutton').click(function () {
         $('#sport').fadeIn(800);
-        $('#men').fadeOut(800);
-        $('#woman').fadeOut(800);
+        $('#men, #woman').fadeOut(800);
     });
 
     $('#menbutton').click(function () {
-        $('#sport').fadeOut(800);
         $('#men').fadeIn(800);
-        $('#woman').fadeOut(800);
+        $('#woman, #sport').fadeOut(800);
     });
 
     $('#womanbutton').click(function () {
-        $('#sport').fadeOut(800);
-        $('#men').fadeOut(800);
         $('#woman').fadeIn(800);
+        $('#sport, #men').fadeOut(800);
     });
 
 });
