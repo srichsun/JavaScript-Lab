792cc20b1b9290f131711643522ea7af1741aff9
Evan <srichsun@gmail.com>
2017-11-27 13:14:09 +0800

    6-2 用jquery觸發事件

--- srichsun.js
+++ srichsun.js
@@ -1,28 +1,8 @@
-window.onload = function () {
+$(function () {
 
-    var target = document.getElementById("numnum"),
-        girl = document.getElementById("girl"),
-        n = 0;
-
-    function show() {
-        target.style.display = "block";
+    function sayit() {
+        alert("Kick you ass!");
     }
 
-    function hide() {
-        target.style.display = "";
-    }
-
-    function addup() {
-        if (n === 6) {
-            n = 0;
-        }
-        n = n + 1;
-        target.innerHTML = n;
-        girl.src = "images/" + n + ".jpg";
-    }
-
-    girl.onmouseover = show;
-    girl.onmouseout = hide;
-    girl.onclick = addup;
-
-};
+    $(".damncolor").on("click", sayit);
+});
