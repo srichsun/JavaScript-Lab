a4a7bfa5c4dbc13494d8c702844d3a7946830929
Evan <srichsun@gmail.com>
2017-11-27 15:04:00 +0800

    6-7-1 nav-bar 點下去menu跑出來
    先把HTML都寫上去，display none，之後點下去要顯示的fadein，其他都fadeout

--- srichsun.js
+++ srichsun.js
@@ -1,46 +1,21 @@
 $(function () {
 
-    //    show
-
-    $("#hide").click(function () {
-        $('#pic').hide(500);
+    $('#sportbutton').click(function () {
+        $('#sport').fadeIn(800);
+        $('#men').fadeOut(800);
+        $('#woman').fadeOut(800);
     });
 
-    $("#toggle").click(function () {
-        $('#pic').toggle(500);
+    $('#menbutton').click(function () {
+        $('#sport').fadeOut(800);
+        $('#men').fadeIn(800);
+        $('#woman').fadeOut(800);
     });
 
-    //    slide
-    $("#slideDown").click(function () {
-        $('#pic').slideDown(500);
+    $('#womanbutton').click(function () {
+        $('#sport').fadeOut(800);
+        $('#men').fadeOut(800);
+        $('#woman').fadeIn(800);
     });
-
-    $("#slideUp").click(function () {
-        $('#pic').slideUp(500);
-    });
-
-    $("#slideToggle").click(function () {
-        $('#pic').slideToggle(500);
-    });
-
-    //    fade
-    $("#fadeIn").click(function () {
-        $('#pic').fadeIn(500);
-    });
-
-    $("#fadeOut").click(function () {
-        $('#pic').fadeOut(500);
-    });
-
-    $("#fadeTo").click(function () {
-        $('#pic').fadeTo(500, 0.5);
-    });
-
-    $("#fadeTo2").click(function () {
-        $('#pic').fadeTo(500, 0.2);
-    });
-
-
-
 
 });
