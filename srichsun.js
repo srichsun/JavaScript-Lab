4aacf4ecb3b0033a941d52fa7e86e5503fa0a4d2
Evan <srichsun@gmail.com>
2017-11-27 14:30:00 +0800

    6-4~6-6 把圖片隱藏顯示、收合、淡入淡出

--- srichsun.js
+++ srichsun.js
@@ -1,6 +1,46 @@
 $(function () {
 
-    $(".damncolor").click(function () {
-    alert("Kick your aAAAAAss!");
-});
+    //    show
+
+    $("#hide").click(function () {
+        $('#pic').hide(500);
+    });
+
+    $("#toggle").click(function () {
+        $('#pic').toggle(500);
+    });
+
+    //    slide
+    $("#slideDown").click(function () {
+        $('#pic').slideDown(500);
+    });
+
+    $("#slideUp").click(function () {
+        $('#pic').slideUp(500);
+    });
+
+    $("#slideToggle").click(function () {
+        $('#pic').slideToggle(500);
+    });
+
+    //    fade
+    $("#fadeIn").click(function () {
+        $('#pic').fadeIn(500);
+    });
+
+    $("#fadeOut").click(function () {
+        $('#pic').fadeOut(500);
+    });
+
+    $("#fadeTo").click(function () {
+        $('#pic').fadeTo(500, 0.5);
+    });
+
+    $("#fadeTo2").click(function () {
+        $('#pic').fadeTo(500, 0.2);
+    });
+
+
+
+
 });
